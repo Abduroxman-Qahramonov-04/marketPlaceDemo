903671e4f88349bca49100e438309a31d8d7434d
Abduroxman-Qahramonov-04 <abdurohman.kahramonov@servpoint.org>
2024-07-24 22:52:42 +0500

    Initial commit

---
+++ src/main/java/com/example/marketPlaceDemo/MarketPlaceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketPlaceDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPlaceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/marketPlaceDemo/MarketPlaceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketPlaceDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
